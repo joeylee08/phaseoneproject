8c221b3480ad2cd4d2ba0dfa057a6e1670fdb088
Kat Tannehill <katetannehill@gmail.com>
2023-09-23 16:19:16 -0600

    create renderEvent function

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
+// Global variables
 const createEventButton = document.querySelector('#createEventBtn');
 const modalBox = document.querySelector('#eventCreateModal');
 const eventForm = document.querySelector('#createEventForm');
+const eventsContainer = document.querySelector("#eventsContainer")
 
 modalBox.classList.add('hidden');
 
+
+
+// Kat code
+const renderEvent = (eventObj) => {
+  const eventCard = document.createElement("div")
+  eventCard.setAttribute("class", "card")
+  const image = document.createElement("img")
+  image.src = eventObj.image
+  image.alt = eventObj.name
+  const h3 = document.createElement("h3")
+  h3.textContent = eventObj.name
+  eventCard.append(image, h3)
+  eventsContainer.append(eventCard)
+}
+
+
+
+// Joseph code
 createEventButton.addEventListener('click', () => {
   modalBox.classList.remove('hidden');
 })
