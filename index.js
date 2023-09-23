cde8f638e73d61ee263363d6cbb49afe75f49dce
Joseph <josephylee72@gmail.com>
2023-09-23 15:19:00 -0700

    added fetch functionality to get all data

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+//global variables
 const createEventButton = document.querySelector('#createEventBtn');
 const modalBox = document.querySelector('#eventCreateModal');
 const eventForm = document.querySelector('#createEventForm');
+const fetchUrl = 'http://localhost:3000/events';
 
+//hide modal box on pageload and attach listener to display it
 modalBox.classList.add('hidden');
-
 createEventButton.addEventListener('click', () => {
   modalBox.classList.remove('hidden');
 })
 
+//close modal if user clicks outside the box
 document.addEventListener('mousedown', (e) => {
   if (e.target === createEventButton) return;
   if (e.target === eventForm) return;
@@ -16,3 +19,17 @@ document.addEventListener('mousedown', (e) => {
     modalBox.classList.add('hidden');
   }
 })
+
+//helper functions
+function getAllData() {
+  fetch(fetchUrl)
+    .then(resp => {
+      if (resp.ok) return resp.json()
+      throw new Error('Failed to fetch event data.')
+    })
+    .then(objArray => objArray.forEach(event => renderEvent(event)))
+    .catch(err => alert(err.message))
+}
+
+//FOR KAT TO CODE!!! DO NOT TOUCH UNLESS YOU ARE KAT!
+function renderEvent(eventObj) {}
