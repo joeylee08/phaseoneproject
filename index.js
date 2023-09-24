fed63bd336200b17c55bf6e01ec8acaa9d7ae894
Joseph <josephylee72@gmail.com>
2023-09-23 17:51:09 -0700

    added POST functionality

--- index.js
+++ index.js
@@ -40,6 +40,7 @@ document.addEventListener('mousedown', (e) => {
 
 eventForm.addEventListener('submit', (e) => {
   e.preventDefault();
+  submitNewEvent()
   alert("Thanks for submitting your event!")
   modalBox.classList.add('hidden')
 })
@@ -53,6 +54,30 @@ function getAllData() {
     })
     .then(objArray => objArray.forEach(event => renderEvent(event)))
     .catch(err => alert(err.message))
+}
+
+function submitNewEvent() {
+  const eventObj = {
+    image: eventForm.image.value,
+    name: eventForm.name.value,
+    date: eventForm.date.value,
+    location: eventForm.location.value,
+    description: eventForm.desc.value
+  }
+  fetch(fetchUrl, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(eventObj)
+  })
+  .then(resp => {
+    if (resp.ok) return resp.json()
+    throw new Error('Failed to submit event data.')
+  })
+  .then(event => renderEvent(event))
+  .catch(err => alert(err.message))
 }
 
 //function call
